ce1e8e1a50dcadf3ac0eb1bd44b06bed1be9d87d
Seang Sokheng <wwhengww@gmail.com>
2024-11-01 22:08:02 +0700

    w4-s1-ex-2 save

--- flutter/lib/w4/w4-s1/EX-2/main.dart
+++ flutter/lib/w4/w4-s1/EX-2/main.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
 
-const String FireBase = "assets/w4-s1-ex/Firebase.jpg";
-const String Dart = "assets/w4-s1-ex/Dart.jpg";
-const String Flutter = "assets/w4-s1-ex/Flutter.jpg";
-enum Dart {}
+enum Product {
+  dart,
+  firebase,
+  flutter,
+}
+
+extension ProductDetails on Product {
+  String get title {
+    switch (this) {
+      case Product.dart:
+        return "Dart";
+      case Product.firebase:
+        return "Firebase";
+      case Product.flutter:
+        return "Flutter";
+    }
+  }
+
+  String get description {
+    switch (this) {
+      case Product.dart:
+        return "The best object language";
+      case Product.firebase:
+        return "The best cloud database";
+      case Product.flutter:
+        return "The best mobile widget libery";
+    }
+  }
+
+  String get image {
+    switch (this) {
+      case Product.dart:
+        return "assets/w4-s1-ex/Dart.jpg";
+      case Product.firebase:
+        return "assets/w4-s1-ex/Firebase.jpg";
+      case Product.flutter:
+        return "assets/w4-s1-ex/Flutter.jpg";
+    }
+  }
+}
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.all(40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+        body: Container(
+          color: Colors.blue,
+          child: Padding(
+            padding:const EdgeInsets.all(40),
+            child: Column(
+              children: [
+                Card(title: Product.dart.title, description: Product.dart.description, image: Product.dart.image),
+                const SizedBox(height: 20,),
+                Card(title: Product.flutter.title, description: Product.flutter.description, image: Product.flutter.image),
+                const SizedBox(height: 20,),
+                Card(title: Product.firebase.title, description: Product.firebase.description, image: Product.firebase.image),
+              ],
+            ),
           ),
         ),
       ),
@@ -22,26 +67,38 @@ void main() {
 
 class Card extends StatelessWidget {
   final String title;
-  final String desc;
-  final 
-  const Card({super.key});
+  final String description;
+  final String image;
+  const Card({required this.title, required this.description, required this.image, super.key});
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        height: 80,
+        height: 200,
         width: 500,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
         ),
         child: Center(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Image.asset(
-                Dart,
+                height: 80,
+                width: 140,
+                image,
               ),
-              Text(),
-              Text(),
+              // const SizedBox(height: 10,),
+              Text(
+                title,
+                style:const TextStyle(
+                  fontSize: 30,
+                
+                ),
+              ),
+              // const SizedBox(height: 10,),
+              Text(description),
             ],
           ),
         ),
